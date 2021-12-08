e75212045060bfd7c0b8dd2f6981cb956c9b6f9d
aayushray <ayushray38@gmail.com>
2021-12-08 21:34:34 +0530

    Initialized

---
+++ home/views.py
@@ -0,0 +1,42 @@
+from django.http.response import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render,redirect
+from .models import *
+import random
+
+# Create your views here.
+
+
+def home(request):
+    context = {'categories' : Category.objects.all()}
+
+    if request.GET.get('category'):
+        return redirect(f"/quiz/?category={request.GET.get('category')}")
+
+    return render(request ,'home.html' ,  context)
+
+def quiz(request):
+    context = {'category' : request.GET.get('category')}
+    return render(request , 'quiz.html' , context)
+
+def get_quiz(request):
+    try:
+        question_objs = list(Question.objects.all())
+        data = []
+        random.shuffle((question_objs))
+
+        print(question_objs)
+
+        for question_obj in question_objs:
+            data.append({
+                "category" : question_obj.category.category_name,
+                "question" : question_obj.question,
+                "marks" : question_obj.marks,
+                "answers" : question_obj.get_answers()
+            })
+        payload = {'status' : True, 'data' : data}
+
+        return JsonResponse(payload)
+    
+    except Exception as e:
+        print(e)
+    return HttpResponse("Something went wrong")
