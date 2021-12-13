e666485d66de6506c531ef5cb6aa2b9e58c33bbd
Ayush Ray <ayushray38@gmail.com>
2021-12-13 16:44:59 +0530

    Delete home directory

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('' , views.home , name="home"),
-    path('api/get-quiz' , views.get_quiz , name="get_quiz"),
-    path('quiz/', views.quiz , name='quiz')
-]
